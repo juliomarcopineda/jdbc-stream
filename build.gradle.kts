13acf9114f12b8125c251c350f6ba0a43eec43bb
juliomarcopineda <juliomarcopineda@gmail.com>
2019-04-30 13:28:37 -0400

    update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,28 +9,18 @@
 version = "0.1.0"
 
 plugins {
-    // Apply the java-library plugin to add support for Java Library
     `java-library`
 }
 
 repositories {
-    // Use jcenter for resolving your dependencies.
-    // You can declare any Maven/Ivy/file repository here.
     jcenter()
 	mavenCentral()
 }
 
-dependencies {
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
+dependencies {	
 
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:27.0.1-jre")
-    // https://mvnrepository.com/artifact/org.xerial/sqlite-jdbc
-    implementation("org.xerial:sqlite-jdbc:3.27.2.1")
-	
-    // Use JUnit test framework
     testImplementation("junit:junit:4.12")
+	testImplementation("org.xerial:sqlite-jdbc:3.27.2.1")
 }
 
 tasks {
